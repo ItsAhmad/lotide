cc6fe57563bd3b517aa4e3362229741b5193977d
ItsAhmad <ahmad.ameen526@hotmail.com>
2024-01-28 20:58:02 -0500

    header function

---
+++ head.js
@@ -0,0 +1,10 @@
+const assertEqual = function(array) {
+  const head = array[0];
+  console.log(head);
+};
+  
+assertEqual([5, 6, 7]);
+assertEqual(["Hello", "Lighthouse", "Labs"]);
+assertEqual([]);
+assertEqual([1]);
+
