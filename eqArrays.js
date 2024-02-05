89ace6e7825f0ca402bdfe38d035fd72d7d2676a
ItsAhmad <ahmad.ameen526@hotmail.com>
2024-02-04 23:06:24 -0500

    made changes to incorporate eqArrays within assertEqual

--- eqArrays.js
+++ eqArrays.js
@@ -1,25 +1,42 @@
-const assertEqual = function(actual, expected) {
+const eqArrays = function(array1, array2) {
 
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-
-};
-
-const eqArrays = function (array1, array2) {
-
-if (array1.length !== array2.length) {
-  return false;
-}
-
-for (let i = 0; i < array1.length; i++) {
-  if (array1[i] !== array2[i]) {
+  if (array1.length !== array2.length) {
     return false;
   }
+  
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  
+  return true;
+  
 };
 
-return true; 
 
+  
+const assertEqual = function(actual, expected) {
+
+  if (Array.isArray(actual) && Array.isArray(expected)) {
+    if (eqArrays(actual, expected)) {
+      console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
+    } else {
+      console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
+    }
+  } else {
+
+    if (actual === expected) {
+      console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+    } else if (actual !== expected) {
+      console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+    }
+  }
 };
+
+
+assertEqual([1, 2, 3], [1, 2, 3]); // => true
+assertEqual([1, 2, 3], [3, 2, 1]); // => false
+
+assertEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
+assertEqual(["1", "2", "3"], ["1", "2", 3]); // => false
