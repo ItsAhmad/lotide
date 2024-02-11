85deaf091f32510c49a0c4505194d7f517e96752
ItsAhmad <ahmad.ameen526@hotmail.com>
2024-02-10 21:03:45 -0500

    fixes

--- countLetters.js
+++ countLetters.js
@@ -1,5 +1,4 @@
 const assertEqual = function(actual, expected) {
-
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else if (actual !== expected) {
@@ -8,39 +7,20 @@ const assertEqual = function(actual, expected) {
 
 };
 
-const assertArraysEqual = function(actual, expected) {
-
-  if (Array.isArray(actual) && Array.isArray(expected)) {
-    if (eqArrays(actual, expected)) {
-      console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
-    } else {
-      console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
-    }
-  } else {
-
-    if (actual === expected) {
-      console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-    } else if (actual !== expected) {
-      console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-    }
-  }
-};
-
-/* 
-Each letter should be placed in the results object 
-each letter is a key, with the value being an array of their position in a string, 0-based
-
-keep in mind some letter appear more than once, adjust the loop for this 
-
-*/
-
-
-const countLetters = function (string) {
+const countLetters = function(string) {
   const results = {};
 
+  for (const letter of string) {
+    if (string[letter]) {
+      if (results[letter]) {
+        results[letter] += 1;
+      } else {
+        results[letter] = 1;
+      }
+    }
+  }
   return results;
-
 };
 
-console.log(countLetters("lighthouse in the house")); 
-
+console.log(countLetters("lighthouse in the house", {'e': true, 'o': true, 'u': true}));
+console.log(countLetters("lighthouse in the house"));
