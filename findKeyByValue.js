b47a53dec240faddfd84909d7469a2643ee03967
ItsAhmad <ahmad.ameen526@hotmail.com>
2024-02-11 15:24:24 -0500

    bug fix

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,11 +1,9 @@
 const assertEqual = function(actual, expected) {
-
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else if (actual !== expected) {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
-
 };
 
 const bestTVShowsByGenre = { 
@@ -14,32 +12,11 @@ const bestTVShowsByGenre = {
   drama:  "The Wire"
 };
 
-const eqObjects = function(object1, object2) {
-  const object1Keys = Object.keys(object1);
-  const object2Keys = Object.keys(object2);
 
-  if (object1Keys.length !== object2Keys.length) {
-    console.log(false);
-    return;
-  }
-
-  for (const key of object1Keys) {
-    if (object1[key] !== object2[key]) {
-      console.log(false);
-      return;
+const findKeyByValue = function (object, value) {
+  for (const key in object) {
+    if (object[key] === value) {
+      return key
     }
   }
-
-  console.log(true);
 };
-
-const shirtObject = { color: "red", size: "medium" };
-const anotherShirtObject= { size: "medium", color: "red" };
-eqObjects(shirtObject , anotherShirtObject); // => true
-
-const longSleeveShirtObject= { size: "medium", color: "red", sleeveLength: "long" };
-eqObjects(shirtObject , longSleeveShirtObject); // => false
-
-
-//assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-//assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
