f3ccaca38c2b50b6b3456f4ed1b070cf67f6058b
ItsAhmad <ahmad.ameen526@hotmail.com>
2024-02-11 15:20:55 -0500

    first part working

--- eqObjects.js
+++ eqObjects.js
@@ -10,26 +10,32 @@ const assertEqual = function(actual, expected) {
 
 
 const eqObjects = function (object1, object2) {
-  const object1keys = object1.keys
-  const object2keys = object2.keys
-
-  if (object1.keys.length === object2.keys.length) {
-    console.log(true)
-  } else {
+  const object1Keys = object1.keys(object1)
+  const object2Keys = object2.keys(object2)
+  if (object1Keys !== object2Keys) {
     console.log(false)
+  } 
+
+  for (const key of object1Keys) {
+    if (object1[key] !== object2[key]) {
+      console.log(false);
+    }
   }
+
+
+    console.log(true);
 };
 
 const shirtObject = { color: "red", size: "medium" };
 const anotherShirtObject= { size: "medium", color: "red" };
-eqObjects(shirtObject , anotherShirtObject); // => true
+eqObjects(shirtObject, anotherShirtObject); // => true
 
 const longSleeveShirtObject= { size: "medium", color: "red", sleeveLength: "long" };
-eqObjects(shirtObject , longSleeveShirtObject); // => false
+eqObjects(shirtObject, longSleeveShirtObject); // => false
 
 
 //assertEqual(eqObjects(shirtObject, anotherShirtObject ), true);
 //assertEqual(eqObjects(shirtObject, longSleeveShirtObject), false);
 
 
-console.log(Object.keys(shirtObject)); // => returns ["color", "size"]
+
--- findKeyByValue.js
+++ findKeyByValue.js
@@ -13,22 +13,33 @@ const bestTVShowsByGenre = {
   comedy: "Brooklyn Nine-Nine",
   drama:  "The Wire"
 };
-const findKeyByValue = function (object, value) {
-  for (const key in object) {
-    if (object[key] === value) {
-      return key
+
+const eqObjects = function(object1, object2) {
+  const object1Keys = Object.keys(object1);
+  const object2Keys = Object.keys(object2);
+
+  if (object1Keys.length !== object2Keys.length) {
+    console.log(false);
+    return;
+  }
+
+  for (const key of object1Keys) {
+    if (object1[key] !== object2[key]) {
+      console.log(false);
+      return;
     }
   }
+
+  console.log(true);
 };
 
+const shirtObject = { color: "red", size: "medium" };
+const anotherShirtObject= { size: "medium", color: "red" };
+eqObjects(shirtObject , anotherShirtObject); // => true
+
+const longSleeveShirtObject= { size: "medium", color: "red", sleeveLength: "long" };
+eqObjects(shirtObject , longSleeveShirtObject); // => false
 
 
-
-
-
-
-
-
-
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+//assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+//assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
