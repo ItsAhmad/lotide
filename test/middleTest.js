db61c78912212a35e1da328b02bc360c169d4132
ItsAhmad <ahmad.ameen526@hotmail.com>
2024-02-20 02:19:03 -0500

    refactoring middle v2

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,16 +1,26 @@
 const middle = require('../middle');
 const assertArraysEqual = require('../assertArraysEqual');
+const eqArrays = require('../eqArrays');
 
 
 
 console.log(middle([1])); // => []
+assertArraysEqual(middle([1]), []);
+
 console.log(middle([1, 2])); // => []
+assertArraysEqual(middle([1, 2]), []);
 
 console.log(middle([1, 2, 3, 4]));
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
 
 console.log(middle([1, 2, 3])); // => [2]
+assertArraysEqual(middle([1, 2, 3]), [2]);
+
 console.log(middle([1, 2, 3, 4, 5])); // => [3]
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
 
 console.log(middle([1, 2, 3, 4])); // => [2, 3]
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+
 console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
 assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
